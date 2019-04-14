f25cba88cf276bed861c828f15ba3be5af8bec6c
unknown <dannyfsociety@gmail.com>
2019-04-13 21:29:13 -0400

    loading news service through ion segnemt :v:

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { IonSegment } from '@ionic/angular';
 import { NewsService } from 'src/app/services/news.service';
+import { Article } from 'src/app/interfaces/interface';
 
 @Component({
   selector: 'app-tab2',
@@ -10,11 +11,24 @@ import { NewsService } from 'src/app/services/news.service';
 export class Tab2Page implements OnInit {
   @ViewChild(IonSegment) segment: IonSegment;
   categories: string[] = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+  news: Article[] = [];
 
   constructor(private newsService: NewsService) { }
 
   ngOnInit() {
     this.segment.value = this.categories[0];
-    this.newsService.getTopHeadlinesCategory('business').subscribe( data => console.log(data));
+    this.loadNews(this.categories[0]);
+  }
+
+  changeCategory(event) {
+    this.news = [];
+    this.loadNews(event.detail.value);
+  }
+
+  private loadNews(category: string) {
+    this.newsService.getTopHeadlinesCategory(category).subscribe( resp => {
+      console.log(resp);
+      this.news.push(...resp.articles);
+    });
   }
 }
